b8cf87823c98f24f42cdd5cbd4751862c908ac50
Jean luc <luctunechi45@gmail.com>
2020-04-19 19:19:08 +0200

    add new changes

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -6,11 +6,11 @@ export const Card = ({ book }) => {
         <div className="card-container">
             <a target="_blank" rel="noopener noreferrer" href={book.volumeInfo.previewLink}>
                 <img src={book.volumeInfo.imageLinks === undefined
-                    ? ""
+                    ? null
                     : `${book.volumeInfo.imageLinks.thumbnail}`} alt={book.volumeInfo.title} />
             </a>
-            <p><span className="text-color">Author:</span>
-                {book.volumeInfo.authors === undefined ? "" : `${book.volumeInfo.authors[0]}`}</p>
+            <p><span className="text-color">Author:</span> {book.volumeInfo.authors === undefined ?
+                null : `${book.volumeInfo.authors[0]}`}</p>
             <p><span className="text-color">Title:</span> {book.volumeInfo.title}</p>
             <p><span className="text-color">Publisher:</span> {book.volumeInfo.publisher}</p>
         </div>
